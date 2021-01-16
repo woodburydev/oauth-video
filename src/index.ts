823751169d91129dcfd04ca9a975a78292f08666
Nathaniel Woodbury <nate_woodbury@trendmicro.com>
2021-01-16 12:07:52 -0800

    Update for HTTPS Part 2

--- src/index.ts
+++ src/index.ts
@@ -26,11 +26,18 @@ mongoose.connect(`${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }))
 
+app.set("trust proxy", 1);
+
 app.use(
   session({
     secret: "secretcode",
     resave: true,
     saveUninitialized: true,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
+    }
   }))
 
 
