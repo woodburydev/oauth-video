f91f3375d0f692a97304e461409d6fbaad23e419
Nathaniel Woodbury <nate_woodbury@trendmicro.com>
2021-01-16 18:01:03 -0800

    Update for HTTPS Part 2

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       sameSite: "none",
-      secure: true,
+      secure: false,
       maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
     }
   }))
