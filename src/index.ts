024b790e69fbd257e5a6af7997227a816fe64f97
Nathaniel Woodbury <nate_woodbury@trendmicro.com>
2021-01-16 18:05:40 -0800

    Update for HTTPS Part 2

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ passport.deserializeUser((id: string, done: any) => {
 passport.use(new GoogleStrategy({
   clientID: `${process.env.GOOGLE_CLIENT_ID}`,
   clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "http://o-auth-video-backend.herokuapp.com/auth/google/callback"
 },
   function (_: any, __: any, profile: any, cb: any) {
 
